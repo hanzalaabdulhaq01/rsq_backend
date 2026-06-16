b590c708b906493fe9490367dffd82789ad8b7f3
hanzalaabdulhaq01 <h13676922@gmail.com>
2026-06-16 17:48:33 +0500

    fix: remove incomplete modules (language, notifications, password-reset) that reference non-existent schema fields

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,17 +18,13 @@ import { DriverPerformanceModule } from './modules/driver-performance/driver-per
 import { ParamedicProfilesModule } from './modules/paramedic-profiles/paramedic-profiles.module';
 import { DriverProfilesModule } from './modules/driver-profiles/driver-profiles.module';
 import { AdminStatsModule } from './modules/admin-stats/admin-stats.module';
-import { PasswordResetModule } from './modules/password-reset/password-reset.module';
-import { LanguageModule } from './modules/language/language.module';
-import { NotificationsModule } from './modules/notifications/notifications.module';
-
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'admin'),
       serveRoot: '/admin',
     }),
-    ConfigModule, CommonModule, AuthModule, UsersModule, OrganizationsModule, AmbulancesModule, RideRequestsModule, DispatchModule, TrackingModule, ChatsModule, AdminActionsModule, DriverPerformanceModule, ParamedicProfilesModule, DriverProfilesModule, AdminStatsModule, PasswordResetModule, LanguageModule, NotificationsModule],
+    ConfigModule, CommonModule, AuthModule, UsersModule, OrganizationsModule, AmbulancesModule, RideRequestsModule, DispatchModule, TrackingModule, ChatsModule, AdminActionsModule, DriverPerformanceModule, ParamedicProfilesModule, DriverProfilesModule, AdminStatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
